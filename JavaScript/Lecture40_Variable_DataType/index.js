b4b5e0868c1fa7d468172aa54f1e547541c3d048
Roy Bipro <roybipro896@gmail.com>
2026-02-21 11:59:18 +0600

    learn about variables and data type

---
+++ JavaScript/Lecture40_Variable_DataType/index.js
@@ -0,0 +1,22 @@
+// var age =26;
+
+// if(true){
+//     console.log(age);
+// }
+
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+
+// solve();
+
+
+{
+    // let is a  block variable
+    let a = 10;
+    console.log(a)
+}
+
+
