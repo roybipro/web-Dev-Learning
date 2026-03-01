42b5444fde29d88cd408592a5765afd17f3bb10c
Roy Bipro <roybipro896@gmail.com>
2026-03-01 19:08:30 +0600

    Leaarn about variable and function hoisting

---
+++ JavaScript/Lecture45/index.js
@@ -0,0 +1,18 @@
+
+
+sayMyname("Bipro")
+
+
+function sayMyname(finalName){
+    console.log(finalName)
+}
+
+
+//when we create a function and and call it before it the function 
+// go to the top of the scope
+//it call function hoisting
+
+
+//variable hoisting it shift he declaration to top of the scope but don't shift the value
+console.log(age);
+var age = 25;
