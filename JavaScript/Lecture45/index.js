b3d46a70f572de24ef17aeb30f0ccd8e8cfc846c
Roy Bipro <roybipro896@gmail.com>
2026-03-01 21:54:28 +0600

    Leaarn about variable and function hoisting

--- JavaScript/Lecture45/index.js
+++ JavaScript/Lecture45/index.js
@@ -1,11 +1,11 @@
 
 
-sayMyname("Bipro")
+// sayMyname("Bipro")
 
 
-function sayMyname(finalName){
-    console.log(finalName)
-}
+// function sayMyname(finalName){
+//     console.log(finalName)
+// }
 
 
 //when we create a function and and call it before it the function 
@@ -14,5 +14,26 @@ function sayMyname(finalName){
 
 
 //variable hoisting it shift he declaration to top of the scope but don't shift the value
-console.log(age);
-var age = 25;
+// console.log(age);
+// var age = 25;
+
+//variable hosting is not working on let or const
+// console.log(age);
+// let age = 25;
+
+//  class Humman{
+
+//  }
+
+//  const object1 = new Humman();
+
+function greetMe(greet,fullName){
+    console.log("Hello",fullName)
+    greet()
+}
+
+function greet(){
+    console.log("Greeting for the day")
+}
+
+greetMe(greet,"Bipro")
