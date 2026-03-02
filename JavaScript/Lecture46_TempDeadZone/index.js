fa84e707ef3f3646ee1e3abb7ab0da997ea66e1f
Roy Bipro <roybipro896@gmail.com>
2026-03-02 18:05:42 +0600

    Learn about temporal dead zone

---
+++ JavaScript/Lecture46_TempDeadZone/index.js
@@ -0,0 +1,15 @@
+
+
+// //inside function
+// function manage(){
+
+// var age = 25;
+// }
+
+// console.log(age)
+
+console.log(marks);
+
+let marks = 50;
+
+//let/const keyword can not hoist it is temporoal dead zone
