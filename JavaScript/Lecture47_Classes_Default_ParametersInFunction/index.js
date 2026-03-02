8cef4af06fd4552c03f628ee841d403b3dd47826
Roy Bipro <roybipro896@gmail.com>
2026-03-02 18:55:53 +0600

    Learn about Default paramiters

--- JavaScript/Lecture47_Classes_Default_ParametersInFunction/index.js
+++ JavaScript/Lecture47_Classes_Default_ParametersInFunction/index.js
@@ -1,40 +1,62 @@
 
 
-class Human {
 
-    //properties
-    age = 23;
-    #wt = 65;   // private can be define by useing #
-    ht = 180;
-
-    constructor(newAge,newHeight,newWaight){
-        this.age = newAge;
-        this.ht = newHeight;
-        this.#wt = newWaight;
-    }
-
-    //behaviour
-    walking(){
-        console.log("I am Walking" ,this.#wt);
-    }
-    running(){
-        console.log("I am Running");
-    }
-
-    get fetchWeight(){
-        return this.#wt;
-    }
-
-    set modifyWaight(val){
-        this.#wt = val;
-    }
-
+function getAge(){
+    return 19
 }
 
+function utility(name = "Bipro", age = getAge()){
+    console.log(name,age);
+}
 
-let obj = new Human(50,190,80);
+utility();
 
-// console.log(obj.#wt);
-obj.walking();
-console.log(obj.ht);
-console.log(obj.fetchWeight);
+// function sayName(fName = "Enter fast Name",lName = "Enter Last name"){
+//     console.log("Myname is :", fName,lName);
+// }
+// sayName("Bipro","Roy");
+
+
+
+
+
+
+
+// class Human {
+
+//     //properties
+//     age = 23;
+//     #wt = 65;   // private can be define by useing #
+//     ht = 180;
+
+//     constructor(newAge,newHeight,newWaight){
+//         this.age = newAge;
+//         this.ht = newHeight;
+//         this.#wt = newWaight;
+//     }
+
+//     //behaviour
+//     walking(){
+//         console.log("I am Walking" ,this.#wt);
+//     }
+//     running(){
+//         console.log("I am Running");
+//     }
+
+//     get fetchWeight(){
+//         return this.#wt;
+//     }
+
+//     set modifyWaight(val){
+//         this.#wt = val;
+//     }
+
+// }
+
+
+// let obj = new Human(50,190,80);
+
+// // console.log(obj.#wt);
+// obj.walking();
+// console.log(obj.ht);
+// console.log(obj.fetchWeight);
