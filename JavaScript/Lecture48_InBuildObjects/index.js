a689f188b9c71f2c8b9bcf2bcb4e1e7f6e1fcc17
Roy Bipro <roybipro896@gmail.com>
2026-03-02 19:23:44 +0600

    Learn about In Built Objects

---
+++ JavaScript/Lecture48_InBuildObjects/index.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+
+console.log(Math.max(12,11,23,41,62,44,88));
+console.log(Math.min(12,11,23,41,62,44,88));
+console.log(Math.round(1.4));
+console.log(Math.floor(1.9));
+console.log(Math.ceil(1.1));
+console.log(Math.abs(-7));
+console.log(Math.sqrt(7));
+console.log(Math.random());
+
+
+
+
+let curr = new Date();
+
+console.log(curr);
+
+
