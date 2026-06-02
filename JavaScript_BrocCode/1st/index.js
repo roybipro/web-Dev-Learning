a2c2ae7d25ac3ba98210d78570036c8d69126010
Roy Bipro <roybipro896@gmail.com>
2026-06-02 22:39:59 +0600

    learnd js variable

--- JavaScript_BrocCode/1st/index.js
+++ JavaScript_BrocCode/1st/index.js
@@ -4,5 +4,28 @@
 // window.alert(`This is an alert`);
 // window.alert(`Fuck off`);
 
-document.getElementById("myh1").textContent = `Hello`;
-document.getElementById("myP").textContent =   `I like pizza`;
+// document.getElementById("myh1").textContent = `Hello`;
+// document.getElementById("myP").textContent =   `I like pizza`;
+
+// let x;
+// let y;
+
+// x = 100;
+
+// console.log(x)
+// let age = 25;
+// let price = 2400;
+// let cgpa = 3.35;
+
+
+// console.log(`You are ${age} years old`);
+// console.log(`The price is ৳ ${price} `);
+// console.log(`Your CGPA is: ${cgpa} `);
+
+let fullName = "Bipro Roy";
+let age = 24;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `You are a student ${isStudent}`;
