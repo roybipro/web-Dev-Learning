337fcd2039449f2d19a07147b51f20951264aac9
Roy Bipro <roybipro896@gmail.com>
2026-06-02 22:03:58 +0600

    learn basic of JS

---
+++ JavaScript_BrocCode/1st/index.js
@@ -0,0 +1,8 @@
+// console.log(`Hello World!`);
+// console.log(`Welcome to JavaScript Bro Code!`);
+
+// window.alert(`This is an alert`);
+// window.alert(`Fuck off`);
+
+document.getElementById("myh1").textContent = `Hello`;
+document.getElementById("myP").textContent =   `I like pizza`;
