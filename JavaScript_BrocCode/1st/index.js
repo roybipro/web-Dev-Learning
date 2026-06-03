6e798f306447612920437b0fcbe4e78ac9358df5
Roy Bipro <roybipro896@gmail.com>
2026-06-03 22:16:03 +0600

    learn basic of JS

--- JavaScript_BrocCode/1st/index.js
+++ JavaScript_BrocCode/1st/index.js
@@ -29,3 +29,5 @@ let isStudent = true;
 document.getElementById("p1").textContent = `Your name is ${fullName}`;
 document.getElementById("p2").textContent = `You are ${age} years old`;
 document.getElementById("p3").textContent = `You are a student ${isStudent}`;
+document.getElementById("p3").textContent = `You are a student: ${isStudent}`;
+
